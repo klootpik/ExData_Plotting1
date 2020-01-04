7dd3f6c5aa14400eba6dc5a02af26726b1ab60a9
klootpik <52610004+klootpik@users.noreply.github.com>
2020-01-04 12:54:32 +0100

    Add files via upload

---
+++ plot1.R
@@ -0,0 +1,73 @@
+library(data.table)
+
+getwd()
+
+destfile <- "./week1/Dataset.zip"
+outDir <- "./week1/uitgepakt"
+urlie <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+
+if(!dir.exists("week1")) {
+  dir.create("week1")
+} 
+
+download.file(urlie, destfile)
+unzip(destfile, exdir=outDir)
+
+file <- list.files(outDir, recursive = T)
+Dataset <- fread(paste0(outDir, "/", file))
+
+# just a check
+Dataset[, .N]
+Dataset[, .N, Date]
+
+# making the subset, only containing data from the dates 2007-02-01 and 2007-02-02
+Databoy <- Dataset[Date %in% c('1/2/2007', '2/2/2007')]
+
+# a check again
+Databoy[, .N, Date]
+str(Databoy)
+
+# I want to create a new variabele that combines date and time in the right format, since I am not very handy 
+# I need a lot of intermediate steps to achieve this
+
+Databoy[, ORG_Date := Date]
+Databoy[, ORG_Time := Time]
+
+Databoy[, Date := strptime(ORG_Date, "%d/%m/%Y")]
+
+# check to check if conversion went right
+Databoy[,.N, .(Date, ORG_Date)]
+str(Databoy)
+
+# combine date and time in one variabele
+Databoy$Supertime <- paste(Databoy$Date, Databoy$Time)
+
+# convert into right format
+Databoy[, Supertime := strptime(Supertime, "%Y-%m-%d %H:%M:%S")]
+
+# final checks on date and time
+class(Databoy$Supertime)
+Databoy[,.N, .(Supertime, Date, Time)]
+
+# Some variables to be plotted must be converted from character to numeric value, otherwise the plots will explode.
+# First a selection of these columns, then defining a function that helps converting, finally an action that
+# makes the actual conversion happen:
+
+columnboys <- names(Databoy)[3:9]
+class(columnboys)
+convertboy <- function(x) as.numeric(x)
+
+# conversion applied to selected columns
+Databoy[,(columnboys):=lapply(.SD, convertboy), .SDcols = columnboys]
+
+# check to see if conversion happened
+str(Databoy)
+
+
+### Now it is finally time to make a plot
+
+# plot 1
+hist(x = Databoy$Global_active_power, main = "Global Active Power", xlab = "Global Active Power (kilowatts)", ylab = "Frequency", col = "red")
+
+dev.copy(png, file = "plot1.png", width = 480, height = 480)
+dev.off()
---
+++ plot3.R
@@ -0,0 +1,83 @@
+library(data.table)
+
+getwd()
+
+destfile <- "./week1/Dataset.zip"
+outDir <- "./week1/uitgepakt"
+urlie <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+
+if(!dir.exists("week1")) {
+  dir.create("week1")
+} 
+
+download.file(urlie, destfile)
+unzip(destfile, exdir=outDir)
+
+file <- list.files(outDir, recursive = T)
+Dataset <- fread(paste0(outDir, "/", file))
+
+# just a check
+Dataset[, .N]
+Dataset[, .N, Date]
+
+# making the subset, only containing data from the dates 2007-02-01 and 2007-02-02
+Databoy <- Dataset[Date %in% c('1/2/2007', '2/2/2007')]
+
+# a check again
+Databoy[, .N, Date]
+str(Databoy)
+
+# I want to create a new variabele that combines date and time in the right format, since I am not very handy 
+# I need a lot of intermediate steps to achieve this
+
+Databoy[, ORG_Date := Date]
+Databoy[, ORG_Time := Time]
+
+Databoy[, Date := strptime(ORG_Date, "%d/%m/%Y")]
+
+# check to check if conversion went right
+Databoy[,.N, .(Date, ORG_Date)]
+str(Databoy)
+
+# combine date and time in one variabele
+Databoy$Supertime <- paste(Databoy$Date, Databoy$Time)
+
+# convert into right format
+Databoy[, Supertime := strptime(Supertime, "%Y-%m-%d %H:%M:%S")]
+
+# final checks on date and time
+class(Databoy$Supertime)
+Databoy[,.N, .(Supertime, Date, Time)]
+
+# Some variables to be plotted must be converted from character to numeric value, otherwise the plots will explode.
+# First a selection of these columns, then defining a function that helps converting, finally an action that
+# makes the actual conversion happen:
+
+columnboys <- names(Databoy)[3:9]
+class(columnboys)
+convertboy <- function(x) as.numeric(x)
+
+# conversion applied to selected columns
+Databoy[,(columnboys):=lapply(.SD, convertboy), .SDcols = columnboys]
+
+# check to see if conversion happened
+str(Databoy)
+
+
+### Now it is finally time to make a plot
+
+# plot 3
+
+# first got to change the language, I am from the Netherdutchlands and there we have different abbreviations for weekdays
+Sys.setlocale("LC_TIME", "English_UK")
+
+with(Databoy, plot(x = Supertime, y = Sub_metering_1, ylab = "Energy sub metering", xlab = "", type = "n"))
+
+with(Databoy, points(x = Supertime, y = Sub_metering_1, type = "l"))
+with(Databoy, points(x = Supertime, y = Sub_metering_2, type = "l", col = "red"))
+with(Databoy, points(x = Supertime, y = Sub_metering_3, type = "l", col = "blue"))
+
+legend("topright", legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty = 1, col = c("black", "red", "blue"))
+
+dev.copy(png, file = "plot3.png", width = 480, height = 480)
+dev.off()
